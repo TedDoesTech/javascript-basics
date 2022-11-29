4259fc1b4207bdf2fb0872089ba64b31b549829f
Ed Crowther <edward930@hotmail.co.uk>
2022-11-29 18:33:07 +0000

    10 boolean tests passed 5 remain

--- src/booleans.js
+++ src/booleans.js
@@ -19,27 +19,27 @@ function one(a, b) {
 };
 
 function truthiness(a) {
-  // your code here
+  return a;
 };
 
 function isEqual(a, b) {
-  // your code here
+  return a===b;
 };
 
 function isGreaterThan(a, b) {
-  // your code here
+  return a>b;
 };
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
+  return a<=b;
 };
 
 function isOdd(a) {
-  // your code here
+  return Math.abs(a % 2) == 1;
 };
 
 function isEven(a) {
-  // your code here
+  return (a % 2) == 0;
 };
 
 function isSquare(a) {
